0e13eeaadf2c599fd0ae8529d152479a5c9118fd
Alexandre Simonin <a.simonin69@gmail.com>
2017-10-26 20:19:44 +0200

    [Fix] utils require

--- config/facebook.js
+++ config/facebook.js
@@ -1,6 +1,6 @@
 let express = require('express'),
   router = express.Router(),
-  utils = require('./utils/utils.js');
+  utils = require('../utils/utils');
 
 // Facebook Webhook
 // Used for verification
